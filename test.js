4223d47ecc3f9ac118a639d1dc47a10dd24af365
jiangwei <jiangwei@chelun.com>
2020-07-28 10:31:38 +0800

    更新几题

--- test.js
+++ test.js
@@ -216,3 +216,181 @@ var removeElement = function(nums, val) {
     }
     return nums;
 };
+
+// 查找字符串在另一个字符串中的索引位置
+var strStr = function(haystack, needle) {
+    if(needle === '') return 0;
+    let index;
+    haystack.replace(new RegExp(needle),(a,b)=>{
+        index = b;
+    })
+    if(index === undefined) return -1
+    return index;
+};
+
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let index = 0;
+    let findIndex = 0;
+    let len =  nums.length;
+    while(index < len){
+        let ele = nums[index];
+        if(index === 0 && target < ele){
+            findIndex = 0;
+            break;
+        }
+        if(ele === target){
+            findIndex = index;
+            break;
+        }else if(target > ele && target < nums[index + 1]){
+            findIndex = index + 1;
+            break;
+        }else{
+            index++;
+            if(index === len) {
+                findIndex = index;
+                break;
+            }
+        }
+        
+    }
+    return findIndex;
+};
+
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和（没做出来）。
+var maxSubArray = function(nums) {
+};
+
+var maxSubArray = function(nums) {
+    if (!nums.length) {
+        return;
+    };
+    // 在每一个扫描点计算以该点数值为结束点的子数列的最大和（正数和）。
+    let max_ending_here = nums[0];
+    let max_so_far = nums[0];
+
+    for (let i = 1; i < nums.length; i ++ ) {
+        // 以每个位置为终点的最大子数列 都是基于其前一位置的最大子数列计算得出,
+        max_ending_here = max_ending_here > 0 ? max_ending_here + nums[i] : nums[i];
+        max_so_far = Math.max ( max_so_far, max_ending_here);
+    };
+
+    return max_so_far;
+};
+
+// console.log(maxSubArray([3, -3, -1, 2 ]));
+
+var lengthOfLastWord = function(s) {
+    let arr = s.split(' ').filter(k => k.length);
+    console.log(arr);
+    let text = arr.pop();
+    return text ? text.length : 0;
+};
+
+/**66. 加一
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ */
+var plusOne = function(digits) {
+    let len = digits.length;
+    let newArr = [];
+
+    function getArr(len){
+        let arr = [];
+        for (let index = 0; index < len; index++) {
+            arr.push(0)
+        }
+        return arr;
+    }
+
+    function next(index){
+        if(index === 0 && (digits[index] + 1)  % 10 === 0) {
+            digits[index] = [1,0];
+            return;
+        };
+        let lastOne = digits[index];
+        if((lastOne + 1) % 10 === 0){
+            let nums = (lastOne + 1) / 10;
+            digits[index] = [...getArr(nums)];
+            index -= 1;
+            next(index)
+        }else{
+            digits[index] = lastOne + 1;
+            return;
+        }
+    }
+
+    next(len - 1)
+
+    function flatten(array){
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            if(Array.isArray(element)){
+                flatten(element)
+            }else{
+                newArr.push(element)
+            }
+        }
+    }
+
+    flatten(digits);
+    
+    return newArr;
+};
+
+/**67. 二进制求和 (没做出来)
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+输入为 非空 字符串且只包含数字 1 和 0。
+ * @param {*} a 
+ * @param {*} b 
+ */
+var addBinary = function(a, b) {
+  
+};
+// [1,1] [1]
+
+//69. x 的平方根,(在一个数组找一个target的问题，都用二分法去做)
+const mySqrt = function(x) {
+    if (x < 2) return x
+    let left = 1, mid, right = Math.floor(x / 2);
+    while (left <= right) {
+       mid = Math.floor(left + (right - left) / 2)
+       if (mid * mid === x) return mid
+       if (mid * mid < x) {
+           left = mid + 1
+       }else {
+           right = mid - 1
+       }
+    }
+    return right
+}
+
+// 70. 爬楼梯
+var climbStairs = function(n) {
+    if(n ===1) return 1;
+    if(n === 2) return 2;
+    return climbStairs(n-1) + climbStairs(n-2)
+};
+
+//350. 两个数组的交集 II
+// nums1 = [1,2,2,1], nums2 = [2,2]
+var intersect = function(nums1, nums2) {
+
+};
+
+//121. 买卖股票的最佳时机（没做出来）
+// [7,1,5,3,6,4]
+var maxProfit = function(prices) {
+    let min = prices[0], max = 0;
+    prices.forEach(p => {
+        if(p < min) min = p;
+        if(max < p - min) max = p - min;
+    })
+    return max
+};
+console.log(maxProfit([1,2,4,1]));
